88f6ce6eb858c1f67bbff6826cc19f360d0d668f
SatanasIdol <kpenao@unal.edu.co>
2023-09-11 04:07:21 -0500

    Final

--- string.js
+++ string.js
@@ -12,27 +12,61 @@ function quitarTildes(text) {
                       return tildes[match];});
 }
 
-//evalua si es palindromo o no
-function palindrom(text){
-  org = text
+// resetea todo el texto
+function resetText(text){
   text = text.replace(regex, '');
   text = quitarTildes(text);
-  text = text.toLowerCase();
-  const invarr = text.split('');
-  invarr.reverse()
-  inv = invarr.join('');
-  (text === inv) ? alert(`Su texto: "${org}" \nES Palindroma`) : alert(`Su texto: "${org}" \nNO es Palindroma`);
+  return text.toLowerCase();
+}
+
+//evalua si es palindromo o no
+function palindrom(text){
+  let textR = resetText(text)
+  const invarr = textR.split('');
+  invarr.reverse();
+  (textR === invarr.join('')) ? alert(`Su texto: "${text}" \nES Palindroma`) : alert(`Su texto: "${text}" \nNO es Palindroma`);
 }
 
 //Mostar en pantalla
+// alert("Hola, este programa le indica si su frase o palabra es palindroma");
 
-alert("Hola, este programa le indica si su frase o palabra es palindroma");
+// // pregunta y valida datos
+// x=true;
+// while(x==true){
+//   let text = prompt("Digite el texto");
+//   (text != '')
+//   ? (palindrom(text),x=false) 
+//   : alert("Porfavor digite un valor valido");
+// }
 
-// pregunta y valida datos
-x=true;
-while(x==true){
-  let text = prompt("Digite el texto");
-  (text != '')
-  ? (palindrom(text),x=false) 
-  : alert("Porfavor digite un valor valido");
+//Anagrama
+
+//Compara si dos textos son anagramas
+function anagramWord(text1,text2){
+  let text1R = resetText(text1);
+  let text2R = resetText(text2);
+  const arr1 = text1R.split('');
+  const arr2 = text2R.split('');
+  arr1.sort();
+  arr2.sort();
+  (arr1.join('') == arr2.join('')) ? alert(`Sus textos: "${text1}" y "${text2}" \nSON Anagramas`) : alert(`Sus textos: "${text1}" y "${text2}" \nNO SON Anagramas`);
 }
+
+//Evalua si en un texto hay anagramas
+// function anagramPhra(text){
+//   textR = text.replace(/[^a-zA-ZñÑáéíóúÁÉÍÓÚüÜ0-9 ]/g, '');
+//   textR = quitarTildes(textR);
+//   textR.toLowerCase();
+//   const arr = textR.split(" ");
+//   const anag = [];
+//   for (let i=0 ; i<arr.length-1 ; i++){
+//     const x = arr[i];
+//     const y = arr[i+1];
+//     console.log(x,y);
+//     // x.join('') == y.join('') ? anag.push[arr2[i]] : null
+//   }
+//   console.log(anag);
+// }
+
+anagramWord("Anagrama ","Amar, gana");
+// anagramPhra("hoy tuve un gran dia muy demasiado es pectacular? yoh");
